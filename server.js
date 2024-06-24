1406e7a847010f2b46e8313bccb99b2fbd91bd22
Maxim <mximsidorov@gmail.com>
2024-06-24 13:29:35 +0300

    http server

--- server.js
+++ server.js
@@ -1,22 +1,11 @@
 const express = require('express');
-const https = require('https');
-const fs = require('fs');
-const path = require('path');
-
 const app = express();
-const port = 443; // HTTPS использует порт 443 по умолчанию
-
-// Настройка путей к сертификатам
-const sslOptions = {
-    key: fs.readFileSync(path.join(__dirname, 'key.pem')),
-    cert: fs.readFileSync(path.join(__dirname, 'cert.pem'))
-};
+const port = 3000; // HTTP обычно использует порт 80, но для локальной разработки используется порт 3000
 
 app.get('/', (req, res) => {
-    res.send('Hello, HTTPS world!');
+    res.send('Hello, HTTP world!');
 });
 
-// Создание HTTPS сервера
-https.createServer(sslOptions, app).listen(port, () => {
-    console.log(`Server is running on https://localhost:${port}`);
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
 });
